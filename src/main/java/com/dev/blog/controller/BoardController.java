07c515d4ff4ed7482bf2ba96c9205a5737004b9f
seolbee <tmf0909@daum.net>
2022-10-04 02:11:49 +0900

    view 엔진 설정 및 로깅 설정

--- src/main/java/com/dev/blog/controller/BoardController.java
+++ src/main/java/com/dev/blog/controller/BoardController.java
@@ -1,11 +1,31 @@
 package com.dev.blog.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/board/")
 public class BoardController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    @RequestMapping(value={"write", "write/{boardIdx}"}, method= RequestMethod.GET)
+    public String writePage(@PathVariable int boardIdx){
+        logger.debug("블로그 글 인덱스 번호 : " + boardIdx);
+        return "pages/board/write";
+    }
 
+    @RequestMapping(value="write", method=RequestMethod.POST)
+    public @ResponseBody Object write(Model model){
+        Map<String, Object> response = new HashMap<>();
+
+        return response;
+    }
 }
--- src/main/java/com/dev/blog/controller/MainController.java
+++ src/main/java/com/dev/blog/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.dev.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
     @RequestMapping(value = {"/", "main"}, method = RequestMethod.GET)
     public String main(){
-        return "index";
+        return "/pages/main/landing";
     }
 }
